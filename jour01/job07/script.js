4d5226377fccd23870373bc4a761386deab5a762
unknown <kenny.ali@laplateforme.io>
2024-04-15 17:15:52 +0200

    job08 fait

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,13 +1,13 @@
 function jourTravaille(date) {
     const joursFeries2024 = [
-        new Date(2024, 0, 1), // Nouvel An
-        new Date(2024, 3, 1), // Fête du Travail
-        new Date(2024, 4, 8), // Victoire des Alliés
-        new Date(2024, 6, 14), // Fête Nationale
-        new Date(2024, 7, 15), // Assomption
-        new Date(2024, 10, 1), // Toussaint
-        new Date(2024, 10, 11), // Armistice
-        new Date(2024, 11, 25) // Noël
+        new Date(2024, 0, 1), 
+        new Date(2024, 3, 1), 
+        new Date(2024, 4, 8), 
+        new Date(2024, 6, 14),
+        new Date(2024, 7, 15), 
+        new Date(2024, 10, 1), 
+        new Date(2024, 10, 11), 
+        new Date(2024, 11, 25) 
     ];
 
     const jourSemaine = date.getDay();
@@ -15,7 +15,7 @@ function jourTravaille(date) {
     const estFerie = joursFeries2024.some(ferie => ferie.getTime() === date.getTime());
 
     const jour = date.getDate();
-    const mois = date.getMonth() + 1; // Les mois sont indexés à partir de 0
+    const mois = date.getMonth() + 1; 
     const annee = date.getFullYear();
 
     if (estFerie) {
@@ -27,5 +27,4 @@ function jourTravaille(date) {
     }
 }
 
-// Exemple d'utilisation de la fonction jourTravaille avec une date donnée
-jourTravaille(new Date(2024, 0, 1)); // Nouvel An (1er janvier 2024)
+jourTravaille(new Date(2024, 0, 1));
