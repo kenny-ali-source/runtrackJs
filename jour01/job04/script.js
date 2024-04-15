a7a869fe504bf4eb4483cfbb505c4a3b5c6c41a7
unknown <kenny.ali@laplateforme.io>
2024-04-15 13:08:49 +0200

    jour de la semaine fait

---
+++ jour01/job04/script.js
@@ -0,0 +1,8 @@
+function afficherJourSemaines(){
+    const semaine = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
+    for(let i = 0; i < semaine.length; i++) 
+    {
+        console.log(semaine[i]);
+    }
+}
+afficherJourSemaines();
