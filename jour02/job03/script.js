6c24a17ab54bf79ad5f5dcc4fb5fef72c593fc7a
unknown <kenny.ali@laplateforme.io>
2024-04-17 13:31:09 +0200

    changement du nom de la fonction

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -2,8 +2,8 @@ const button = document.getElementById('button');
 const compteur = document.getElementById('compteur');
 let number = 0;
 
-function mettreAJourCompteur() {
+function addOne() {
     number++;
     compteur.textContent = number;
 }
-button.addEventListener('click', mettreAJourCompteur);
+button.addEventListener('click', addOne);
